b906aa8320faa7aef217afe1a70bbeaec3bcea73
Rajnish Kumar Pandey <59505807+PandeyRajnish@users.noreply.github.com>
2025-02-13 09:45:05 +0530

    add loading spinner and loading page component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,4 +1,8 @@
-const HomePage = () => {
+// Testing loading spinner
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+const HomePage = async () => {
+  await delay(5000);
   return <>Prostore</>;
 };
 
