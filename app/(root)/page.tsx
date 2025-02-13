1590384b32ce4ccb4915b6414c6ec32732c73e40
Rajnish Kumar Pandey <59505807+PandeyRajnish@users.noreply.github.com>
2025-02-13 10:11:45 +0530

    remove loading spinner test and add custom 404 Not Found page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,4 @@
-// Testing loading spinner
-const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
-
 const HomePage = async () => {
-  await delay(5000);
   return <>Prostore</>;
 };
 
